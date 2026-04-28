1767eb04869bda4ca72197618150156a1084ffaf
alvescostapriscila@gmail.com <alvescostapriscila@gmail.com>
2026-04-28 11:06:56 -0300

    Alteração regra negócio \services\withdrawalService.js linha 19

--- src/services/withdrawalService.js
+++ src/services/withdrawalService.js
@@ -16,7 +16,7 @@ function getCurrentDate() {
 function createUserWithdrawal(userId, payload) {
   const quantity = Number(payload.quantity);
 
-  if (quantity !== 1) {
+  if (quantity !== 0 ) {
     throw new HttpError(400, "Quantidade invalida para retirada.", [
       {
         field: "quantity",
